b540e71b8d59285d06dd931eea04bd8f72637812
Rodrigo Arenas Catalán <rodare.cl@gmail.com>
2021-03-13 15:43:02 +0100

    first

---
+++ main.js
@@ -0,0 +1,321 @@
+
+function toggleContent1() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content1");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+
+function toggleContent2() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content2");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent3() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content3");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent4() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content4");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent5() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content5");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent6() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content6");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent7() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content7");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent8() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content8");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent9() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content9");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent10() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content10");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent11() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content11");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent12() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content12");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent13() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content13");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent14() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content14");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent15() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content15");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent16() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content16");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent17() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content17");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent18() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content18");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent19() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content19");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent20() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content20");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent21() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content21");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent22() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content22");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent23() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content23");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent24() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content24");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent25() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content25");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent26() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content26");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+function toggleContent27() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content27");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+function toggleContent28() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content28");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+function toggleContent29() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content29");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+function toggleContent30() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content30");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+function toggleContent31() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content31");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+function toggleContent32() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content32");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+function toggleContent33() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content33");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent34() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content34");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent35() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content35");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent36() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content36");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent37() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content37");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent38() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content38");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent39() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content39");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+
+function toggleContent40() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content40");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
+function toggleContent41() {
+  // Get the DOM reference
+  var contentId = document.getElementById("content41");
+  // Toggle 
+  contentId.style.display == "block" ? contentId.style.display = "none" : 
+contentId.style.display = "block"; 
+}
